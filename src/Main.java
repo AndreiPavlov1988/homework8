63a94b8750bc25aa957fec5c4944a42defb07ed6
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-20 12:58:18 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] inputArray1 = {15000, 22000, 18000, 25000, 19000};
@@ -13,5 +14,12 @@ public class Main {
         double[] outputArray1 = {sum, max, min, average};
         System.out.println("inputArray1: " + java.util.Arrays.toString(inputArray1));
         System.out.println("outputArray1: " + java.util.Arrays.toString(outputArray1));
+    //task 2
+        int[] inputArray2 = {50000, 75000, 60000, 80000, 65000};
+        double[] outputArray2 = new double[inputArray2.length];
+        for (int i = 0; i < inputArray2.length; i++) {
+            outputArray2[i] = inputArray2[i] * 0.13;}
+        System.out.println("inputArray2: " + Arrays.toString(inputArray2));
+        System.out.println("outputArray2: " + Arrays.toString(outputArray2));
     }
 }
